fce1e26318b9e870787c955908ba6270141da2a3
CooLMinE <CooLMinE@users.noreply.github.com>
2013-11-10 07:33:46 +0000

    initial commit.

---
+++ MouseHookLib/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace MouseHookLib
+{
+    /// <summary>
+    /// Windows mouse messages.
+    /// </summary>
+    internal enum MouseMessages
+    {
+        WM_MOUSEMOVE = 0x0200,
+        WM_LBUTTONDOWN = 0x0201,
+        WM_LBUTTONUP = 0x0202,
+        WM_RBUTTONDOWN = 0x0204,
+        WM_RBUTTONUP = 0x0205,
+        WM_MOUSEWHEEL = 0x020A,
+    }
+}
---
+++ MouseHookLib/Structs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MouseHookLib
+{
+    /// <summary>
+    /// The POINT structure defines the x- and y- coordinates of a point.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct POINT
+    {
+        public int x;
+        public int y;
+    }
+
+    /// <summary>
+    /// Contains information about a low-level mouse input event. 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+}
